82e58c6f8d322e65647f4949ac34bce51e1b9752
Julien Rhodes <julien.rhodes@gmail.com>
2014-04-13 00:44:28 -0700

    First checkin

---
+++ ArduinoRF_SMD.cmd
@@ -0,0 +1,152 @@
+# Pin name action command file
+
+# Start of element U2
+ChangePinName(U2, 5, nc)
+ChangePinName(U2, 4, NC)
+ChangePinName(U2, 7, 7)
+ChangePinName(U2, 6, 6)
+ChangePinName(U2, 3, 3)
+ChangePinName(U2, 1, 1)
+ChangePinName(U2, 2, 2)
+ChangePinName(U2, 8, 8)
+
+# Start of element D2
+ChangePinName(D2, 3, 3)
+ChangePinName(D2, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 2, E)
+ChangePinName(Q1, 3, C)
+ChangePinName(Q1, 1, B)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 5, R4A)
+ChangePinName(R4, 4, R4)
+ChangePinName(R4, 6, R3A)
+ChangePinName(R4, 3, R3)
+ChangePinName(R4, 7, R2A)
+ChangePinName(R4, 2, R2)
+ChangePinName(R4, 8, R1A)
+ChangePinName(R4, 1, R1)
+
+# Start of element R3
+ChangePinName(R3, 5, R4A)
+ChangePinName(R3, 4, R4)
+ChangePinName(R3, 6, R3A)
+ChangePinName(R3, 3, R3)
+ChangePinName(R3, 7, R2A)
+ChangePinName(R3, 2, R2)
+ChangePinName(R3, 8, R1A)
+ChangePinName(R3, 1, R1)
+
+# Start of element RESET
+ChangePinName(RESET, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 1, cathode)
+ChangePinName(D1, 2, anode)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element Z1
+ChangePinName(Z1, 1, 1)
+ChangePinName(Z1, 3, 3)
+
+# Start of element U1
+ChangePinName(U1, 22, ADC7)
+ChangePinName(U1, 19, ADC6)
+ChangePinName(U1, 6, VCC)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 11, "PD7 (PCINT23/AIN1)")
+ChangePinName(U1, 10, "PD6 (PCINT22/OC0A/AIN0)")
+ChangePinName(U1, 9, "PD5 (PCINT21/OC0B/T1)")
+ChangePinName(U1, 2, "PD4 (PCINT20/XCK/T0)")
+ChangePinName(U1, 1, "PD3 (PCINT19/OC2B/INT1)")
+ChangePinName(U1, 32, "PD2 (INT0/PCINT18)")
+ChangePinName(U1, 31, "PD1 (TXD/PCINT17)")
+ChangePinName(U1, 30, "PD0 (RXD/PCINT16)")
+ChangePinName(U1, 8, "PB7 (PCINT7/XTAL2/TOSC2)")
+ChangePinName(U1, 7, "PB6 (PCINT6/XTAL1/TOSC1)")
+ChangePinName(U1, 17, "PB5 (SCK/PCINT5)")
+ChangePinName(U1, 16, "PB4 (PCINT4/MISO)")
+ChangePinName(U1, 15, "PB3 (PCINT3/OC2A/MOSI)")
+ChangePinName(U1, 14, "PB2 (PCINT2/OC1B)")
+ChangePinName(U1, 13, "PB1 (PCINT1/OC1A)")
+ChangePinName(U1, 12, "PB0 (PCINT0/CLK0/ICP1)")
+ChangePinName(U1, 29, "(_RESET_/PCINT14) PC6")
+ChangePinName(U1, 28, "(ADC5/PCINT13) PC5")
+ChangePinName(U1, 27, "(ADC4/PCINT12) PC4")
+ChangePinName(U1, 26, "(ADC3/PCINT11) PC3")
+ChangePinName(U1, 25, "(ADC2/PCINT10) PC2")
+ChangePinName(U1, 24, "(ADC1/PCINT9) PC1")
+ChangePinName(U1, 23, "(ADC0/PCINT8) PC0")
+ChangePinName(U1, 18, AVCC)
+ChangePinName(U1, 20, AREF)
+ChangePinName(U1, 21, GND)
+ChangePinName(U1, 3, GND)
+ChangePinName(U1, 4, VCC)
+
+# Start of element CONN4
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 2, 2)
+ChangePinName(CONN4, 1, 1)
+
+# Start of element CONN2
+ChangePinName(CONN2, 10, 10)
+ChangePinName(CONN2, 9, 9)
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 12, 12)
+ChangePinName(CONN3, 11, 11)
+ChangePinName(CONN3, 10, 10)
+ChangePinName(CONN3, 9, 9)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+
+# Start of element CONN_PWR
+ChangePinName(CONN_PWR, 1, 1)
+ChangePinName(CONN_PWR, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
